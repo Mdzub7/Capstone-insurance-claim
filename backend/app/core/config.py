45c808358148d1c9e21e712db6d756dc1857b2e1
EC2 Default User <ec2-user@ip-10-105-194-191.ec2.internal>
2025-11-30 06:36:58 +0000

    Basic Model 0-1

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     AWS_REGION: str = "eu-west-2"
     # These names must match your Terraform Outputs!
     DYNAMODB_TABLE: str = "intl-capstone-team2-claims-dev" 
-    S3_BUCKET: str = "intl-euro-capstone-team2" # Update this with your actual bucket name
+    S3_BUCKET: str = "intl-euro-capstone-team2-dev" # Update this with your actual bucket name
     JWT_SECRET_NAME: str = "jwt_secret"
     JWT_ALGORITHM: str = "HS256"
     ADMIN_EMAIL: str = "admin@healthcare.com"
